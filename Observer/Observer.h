b14ceec7ab5647f4e74bfc8031035c8185a8677c
Daniel Coleman <dcoleman0711@gmail.com>
2021-11-18 12:10:57 -0600

    Initial Commit

---
+++ Observer/Observer.h
@@ -0,0 +1,18 @@
+//
+//  Observer.h
+//  Observer
+//
+//  Created by Daniel Coleman on 11/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Observer.
+FOUNDATION_EXPORT double ObserverVersionNumber;
+
+//! Project version string for Observer.
+FOUNDATION_EXPORT const unsigned char ObserverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Observer/PublicHeader.h>
+
+
